15d9b9201e0498ddfde7eddb7c71b12b682f5dee
Max Lapides <max.lapides@tonal.com>
2019-04-10 13:22:39 -0700

    fix: top nav indicators for real

--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,41 @@ import { globalHistory } from "@reach/router"
 
 import { Join } from "./join"
 
-const Header = ({ menuLinks }) => (
-  <AntLayout.Header>
-    <Menu mode="horizontal" selectedKeys={[globalHistory.location.pathname]}>
-      {menuLinks.map(link => (
-        <Menu.Item key={link.link}>
-          <Link to={link.link}>{link.name}</Link>
-        </Menu.Item>
-      ))}
-      <Menu.Item key="call">
-        <a
-          href="https://socialistcall.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          The Call
-        </a>
-      </Menu.Item>
-      <Menu.Item key="join">
-        <Join />
-      </Menu.Item>
-    </Menu>
-  </AntLayout.Header>
-)
+class Header extends React.Component {
+  state = {
+    selected: globalHistory.location.pathname,
+  }
+
+  handleClick = key => this.setState({ selected: key })
+
+  render() {
+    return (
+      <AntLayout.Header>
+        <Menu mode="horizontal" selectedKeys={[this.state.selected]}>
+          {this.props.menuLinks.map(link => (
+            <Menu.Item
+              key={link.link}
+              onClick={() => this.handleClick(link.link)}
+            >
+              <Link to={link.link}>{link.name}</Link>
+            </Menu.Item>
+          ))}
+          <Menu.Item key="call">
+            <a
+              href="https://socialistcall.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              The Call
+            </a>
+          </Menu.Item>
+          <Menu.Item key="join">
+            <Join />
+          </Menu.Item>
+        </Menu>
+      </AntLayout.Header>
+    )
+  }
+}
 
 export default Header

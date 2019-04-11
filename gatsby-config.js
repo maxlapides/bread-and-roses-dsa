36c0262525cc0ce1a150d715ee0940377eda1af9
Max Lapides <max.lapides@tonal.com>
2019-04-11 09:43:43 -0700

    add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,13 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-TF5DRZ7",
+        includeInDevelopment: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
